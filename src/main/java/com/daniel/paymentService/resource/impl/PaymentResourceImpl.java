31511349a379af32bf9cff17002045d3d6f4038b
Daniel Borges <lemebor91@yahoo.com>
2023-05-01 22:36:00 -0300

    subindo projeto envio de objeto tópico kafka

---
+++ src/main/java/com/daniel/paymentService/resource/impl/PaymentResourceImpl.java
@@ -0,0 +1,28 @@
+package com.daniel.paymentService.resource.impl;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.paymentService.model.Payment;
+import com.daniel.paymentService.resource.PaymentResource;
+import com.daniel.paymentService.service.PaymentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResourceImpl implements PaymentResource {
+	
+	private final PaymentService paymentService;
+
+	@Override
+	public ResponseEntity<Payment> payment(Payment payment) {
+	 paymentService.sendPayment(payment);
+	 return ResponseEntity.status(HttpStatus.CREATED).build();
+		
+	}
+
+}
---
+++ src/main/java/com/daniel/paymentService/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.daniel.paymentService.service;
+
+import com.daniel.paymentService.model.Payment;
+
+public interface PaymentService {
+	
+	void sendPayment(Payment payment);
+
+}
